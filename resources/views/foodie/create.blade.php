16c6e5baac4bebb99dac3bbbd809e385c47a58fb
HIROKO HYAKKENDANI <hiro_love_avril-6.ghol-17@ezweb.ne.jp>
2021-12-09 14:24:05 +0000

    ログイン画面の編集

---
+++ resources/views/foodie/create.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.common')
+@section('title', 'お店の新規登録')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9 mx-auto">
+                <h2>お店の新規登録</h2>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('foodie/create','FoodieController@add')->middleware('auth'); 
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
