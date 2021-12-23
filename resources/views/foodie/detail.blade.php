9dd9f717825bd188cda7bdfc50e593b9bda72ee4
HIROKO HYAKKENDANI <hiro_love_avril-6.ghol-17@ezweb.ne.jp>
2021-12-23 03:01:02 +0000

    お店の詳細画面　トップ

--- resources/views/foodie/detail.blade.php
+++ resources/views/foodie/detail.blade.php
@@ -19,30 +19,105 @@
                    
                 </div>
             </div>
+            <!--ナビゲーションタブの実装-->
             <div class="row">
-                    <ul class="nav nav-tabs nav-pills nav-fill col-md-9">
-                        <li class="nav-item bg-danger">
-                            <a class="nav-link bg-danger active" id="pills-top-tab" data-togle="pill" href="#pills-top" role="tab" area-controls="pills-top" area-selected="true">トップ</a>
+                    <ul class="nav nav-pills mb-5 nav-fill col-md-12" id="pills-tab" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" id="pills-top-tab" data-toggle="pill" href="#pills-top" role="tab" aria-controls="pills-top" aria-selected="true">トップ</a>
                         </li>
-                        <li class="nav-item bg-danger">
-                            <a class="nav-link" id="pills-seat-tab" data-toggle="pill" href="#pills-seat" role="tab" area-controls="pills-seat" area-selected="false">座席</a>
+                        <li class="nav-item">
+                            <a class="nav-link" id="pills-seat-tab" data-toggle="pill" href="#pills-seat" role="tab" aria-controls="pills-seat" aria-selected="false">座席</a>
                         </li>
-                        <li class="nav-item bg-danger">
-                            <a class="nav-link" id="pills-menu-tab" data-toggle="pill" href="#pills-menu" role="tab" area-controls="pills-menu" area-selected="false">メニュー</a>
+                        <li class="nav-item">
+                            <a class="nav-link" id="pills-menu-tab" data-toggle="pill" href="#pills-menu" role="tab" aria-controls="pills-menu" aria-selected="false">メニュー</a>
                         </li>
-                        <li class="nav-item bg-danger">
-                            <a class="nav-link" id="pills-map-tab" data-toggle="pill" href="#pills-map" role="tab" area-controls="pills-map" area-selected="false">地図</a>
+                        <li class="nav-item">
+                            <a class="nav-link" id="pills-map-tab" data-toggle="pill" href="#pills-map" role="tab" aria-controls="pills-map" aria-selected="false">地図</a>
                         </li>
-                        <li class="nav-item bg-danger">
-                            <a class="nav-link" id="pills-image-tab "data-toggle="pill" href="#pills-image" role="tab" area-controls="pills-image" area-selected="false">画像</a>
+                        <li class="nav-item">
+                            <a class="nav-link" id="pills-image-tab " data-toggle="pill" href="#pills-image" role="tab" aria-controls="pills-image" aria-selected="false">画像</a>
                         </li>
-                        <li class="nav-item bg-danger">
-                            <a class="nav-link" id="pills-comment-tab "data-toggle="pill" href="#pills-comment" role="tab" area-controls="pills-comment" area-selected="false">口コミ</a>
+                        <li class="nav-item">
+                            <a class="nav-link" id="pills-comment-tab " data-toggle="pill" href="#pills-comment" role="tab" aria-controls="pills-comment" aria-selected="false">口コミ</a>
                         </li>
-                    </ul> 
-                    <div class="tab-content col-md-4" id="pills-tabContent">
-                        <div class="tab-pane fade show active" id="pills-top" role="tabpanel" aria-labelledby="pills-top-tab">基本情報</div>
-                        <div class="tab-pane fade" id="pills-seat" role="tabpanel" aria-labelledby="pills-seat-tab">席の種類　席数</div>
+                    </ul>
+                    <!--トップ-->
+                    <div class="tab-content" id="pills-tabContent">
+                        <div class="tab-pane fade show active mb-3" id="pills-top" role="tabpanel" aria-labelledby="pills-top-tab">
+                            <div class="d-flex flex-row bd-highlight col-md-5 m-3">
+                                <img src="../images/cake.jpg" class="img-fluid float-left pr-3">
+                                <img src="../images/cake.jpg" class="img-fluid float-left pr-3">
+                                <img src="../images/cake.jpg" class="img-fluid float-left pr-3">
+                            </div>    
+                            <div class="d-flex flex-row bd-highlight col-md-5 m-3">
+                                <img src="../images/cake.jpg" class="img-fluid float-left pr-3">
+                                <img src="../images/cake.jpg" class="img-fluid float-left pr-3">
+                                <img src="../images/cake.jpg" class="img-fluid float-left pr-3">
+                            </div>
+                            <table class="table table-bordered p-3">
+                                <h3 class="mt-5">基本情報</h3>
+                                <tbody>
+                                    <tr>
+                                        <th class="table_headline">店名</th>
+                                        <td class="shop_info">
+                                            <sapn>お店の名前</sapn>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th class="table_headline">予算</th>
+                                        <td classs="shop_info">
+                                            <span>（例）1000円～2000円</span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th class="table_headline">住所</th>
+                                        <td class="shop_info">
+                                            <sapn>郵便番号・住所・アクセス情報が入ります</sapn>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th class="table_headline">座席</th>
+                                        <td class="shop_info">
+                                            <sapn>席数。カウンター席〇席、テーブル席〇席</sapn>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th class="table_headline">メニュー</th>
+                                        <td class="shop_info">
+                                            <sapn>料理：料理の種類　ドリンク：ドリンクの種類</sapn>
+                                        </td>
+                                    </tr>
+                                     <tr>
+                                        <th class="table_headline">お問い合わせ番号</th>
+                                        <td class="shop_info">
+                                            <sapn>000</sapn>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th class="table_headline">営業時間</th>
+                                        <td class="shop_info">
+                                            <sapn>何時～何時</sapn>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th class="table_headline">交通手段</th>
+                                        <td class="shop_info">
+                                            <sapn>〇〇線　△改札口～徒歩何分</sapn>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th class="table_headline">支払い方法</th>
+                                        <td class="shop_info">
+                                            <sapn>現金。クレカ</sapn>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="tab-pane fade" id="pills-seat" role="tabpanel" aria-labelledby="pills-seat-tab">
+                            <table class="table table-bordered p-3">
+                                <h3 class="mt-5">席の種類・席数</h3>
+                        </div>
                         <div class="tab-pane fade" id="pills-menu" role="tabpanel" aria-labelledby="pills-menu-tab">メニュー</div>
                         <div class="tab-pane fade" id="pills-map" role="tabpanel" aria-labelledby="pills-map-tab">住所</div>
                         <div class="tab-pane fade" id="pills-image" role="tabpanel" aria-labelledby="pills-image-tab">口コミ投稿画像</div>
